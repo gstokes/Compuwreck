793e292d36dbe7f8170ba9e28c82ab5a734c8d04
gstokes <gavinstokes@gmail.com>
2015-08-30 19:59:36 +0100

    photo

--- Compuwreck.Core/Model/Location.cs
+++ Compuwreck.Core/Model/Location.cs
@@ -4,6 +4,15 @@ using System.Linq;
 using System.Text;
 
 namespace Compuwreck.Core.Model {
-    class Location {
+    public class Location {
+        public int LocationId { get; private set; }
+        public int ShipwreckId { get; private set; }
+        public double? LTD { get; private set; }
+        public double? LNG { get; set; }
+        public string Lat { get; private set; }
+        public string Long { get; private set; }
+        public System.Data.Entity.Spatial.DbGeography GeoLocation { get; private set; }
+
+        public virtual Shipwreck Shipwreck { get; set; }
     }
 }
--- Compuwreck.Core/Model/Photo.cs
+++ Compuwreck.Core/Model/Photo.cs
@@ -4,6 +4,15 @@ using System.Linq;
 using System.Text;
 
 namespace Compuwreck.Core.Model {
-    class Photo {
+    public class Photo {
+        public int Photo_id { get; private set; }
+        public string Name { get; private set; }
+        public string Details { get; private set; }
+        public string FileName { get; private set; }
+        public string URL { get; private set; }
+        public string Source { get; private set; }
+        public int Shipwreck_id { get; private set; }
+
+        public virtual Shipwreck Shipwreck { get; private set; }
     }
 }
