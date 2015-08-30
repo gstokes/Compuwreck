2fd453d2636efb150dce16812d7d5b9944f5074c
gstokes <gavinstokes@gmail.com>
2015-08-30 20:05:21 +0100

    models

---
+++ Compuwreck.Core/Model/Event.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+
+namespace Compuwreck.Core.Model {
+    public class Event {
+        public int EventId { get; private set; }
+        public string EventName { get; private set; }
+        public string EventDescription { get; private set; }
+    }
+}
---
+++ Compuwreck.Core/Model/Reference.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compuwreck.Core.Model {
+   public class Reference {
+       public int ID { get; private set; }
+       public string AbreviatedTitle { get; private set; }
+       public string LongTitle { get; private set; }
+       public string Author { get; private set; }
+       public string PublisherSource { get; private set; }
+       public string PublishersAddress { get; private set; }
+       public string Source { get; private set; }
+       public string DatePublished { get; private set; }
+       public string RelevantMaterial { get; private set; }
+       public string ISBN { get; private set; }
+    }
+}
