214d74fd870a9ee9384d682974e643f860812506
gstokes <gavinstokes@gmail.com>
2015-08-30 20:35:02 +0100

    County

---
+++ Compuwreck.Core/Model/County.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+
+namespace Compuwreck.Core.Model {
+    public class County {
+        public int CountyId { get; private set; }
+        public string CountyName { get; private set; }
+    }
+}
